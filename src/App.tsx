fee6a61d454deb199ff205092931120fb4ad38fd
Rique2 <enrique2heinrich@gmail.com>
2023-05-05 12:10:50 +0200

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ const loadGames = async (search = '') => {
 };
 
 function App() {
-  const [games, setGames] = useState<Game[] | null>(null);
   const [cartItems, setCartItems] = useState<Game[]>([]);
   const [isCartOpen, setIsCartOpen] = useState(false);
   const location = useLocation();
@@ -47,7 +46,7 @@ function App() {
         <Route path='*' element={<NotFound />} />
           <Route
             path="/"
-            element={<Home setGames={setGames} loadGames={loadGames} />}
+            element={<Home loadGames={loadGames} />}
 
           />
           <Route path="games">
@@ -55,8 +54,6 @@ function App() {
               index
               element={
                 <GameList
-                  games={games}
-                  setGames={setGames}
                   loadGames={loadGames}
                   cartItems={cartItems}
                   addToCart={addToCart}
@@ -67,7 +64,6 @@ function App() {
               path=":gameId"
               element={
                 <GameDetails
-                  games={games}
                   cartItems={cartItems}
                   addToCart={addToCart}
                 />
